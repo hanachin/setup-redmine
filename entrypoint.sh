e4857858317448769e3544703ffa059dc4200039
Seiei Miyagi <hanachin@gmail.com>
2019-09-12 09:47:51 +0900

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 
 if [ $1 = trunk ]; then
   svn co --non-interactive --config-option servers:global:ssl-authority-files=<(curl -sL https://www.gandi.net/static/CAs/GandiStandardSSLCA2.pem) https://svn.redmine.org/redmine/trunk redmine-trunk
+  cd redmine-trunk
   exit
 fi
 if [ ! -d redmine-$1 ]; then
   curl https://redmine.org/releases/redmine-$1.tar.gz | tar zx
 fi
+
+cd redmine-$1
