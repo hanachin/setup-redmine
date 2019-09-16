b9d833ca5a9606092fcc4f617efe2c9a52c4d2e7
Seiei Miyagi <hanachin@gmail.com>
2019-09-16 23:43:48 +0900

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ set -e
 if [ $1 = trunk ]; then
   svn co --non-interactive --config-option servers:global:ssl-authority-files=<(curl -sL https://www.gandi.net/static/CAs/GandiStandardSSLCA2.pem) https://svn.redmine.org/redmine/trunk redmine-trunk
 else
-  curl https://redmine.org/releases/redmine-$1.tar.gz | tar zx
+  curl -s https://redmine.org/releases/redmine-$1.tar.gz | tar zx
 fi
 
 mv redmine-$1 /tmp
 
 if [ -z $2 ]; then
-  rsync -r /tmp/redmine-$1 /github/workspace
+  rsync -av /tmp/redmine-$1 /github/workspace
 else
   mv /tmp/redmine-$1 $2
 fi
